06ce3cf1d12dba13c72fb9e8575d2c47c6ba970a
Sal-satar <faisal.jtg2010@gmail.com>
2021-02-01 21:36:02 -0500

    my first commit

--- src/test/java/com/trycloud/tests/user_story1/User_story1.java
+++ src/test/java/com/trycloud/tests/user_story1/User_story1.java
@@ -1,2 +1,4 @@
-package com.trycloud.tests.user_story1;public class User_story1 {
+package com.trycloud.tests.user_story1;
+
+public class User_story1 {
 }
---
+++ src/test/java/com/trycloud/tests/user_story5/User_story5.java
@@ -0,0 +1,7 @@
+package com.trycloud.tests.user_story5;
+
+public class User_story5 {
+    public static void main(String[] args) {
+        System.out.println("hello team;)");
+    }
+}
