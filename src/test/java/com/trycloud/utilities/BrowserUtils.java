5e70c62ff85ce8a458bcf97ba79db647b0738669
ayyuksel <74163951+ayyuksel@users.noreply.github.com>
2021-02-04 07:26:51 -0500

    user story 2

--- src/test/java/com/trycloud/utilities/BrowserUtils.java
+++ src/test/java/com/trycloud/utilities/BrowserUtils.java
@@ -1,4 +1,13 @@
 package com.trycloud.utilities;
 
 public class BrowserUtils {
+    public static void sleep(int second){
+        second *= 1000;
+        try {
+            Thread.sleep(second);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("Something happened in sleep method");
+    }
 }
