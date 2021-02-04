147bd4ee9021c3b12dbeb294c0e59a2f86ece013
Ayushjav13 <48998992+Ayushjav13@users.noreply.github.com>
2021-02-04 16:41:11 -0500

    Created Driver class to open and set-up the browser

--- src/test/java/com/trycloud/tests/user_story3/User_story3_2.java
+++ src/test/java/com/trycloud/tests/user_story3/User_story3_2.java
@@ -1,4 +1,15 @@
 package com.trycloud.tests.user_story3;
 
+import org.testng.annotations.Test;
+
 public class User_story3_2 {
+    //5-AC:verify users can upload a file directly to the homepage (File-Uploading)
+    //6-AC:verify users can create a folder
+    //7-AC:verify users can upload a file inside a folder.
+    //8-AC:verify users can delete a file/folder.
+
+    @Test(description = "verify users can upload a file directly to the homepage (File-Uploading)")
+    public void test1(){
+
+    }
 }
